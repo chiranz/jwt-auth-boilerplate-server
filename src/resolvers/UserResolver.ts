a843690ef803369a819ad22cce7ecf773b1eecc2
chiranz <chiranpoudyal@gmail.com>
2020-02-07 13:17:25 +0530

    Login mutation with jwt refresh token

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,6 +1,20 @@
-import { Resolver, Query, Mutation, Arg } from "type-graphql";
-import { hash } from "bcryptjs";
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  ObjectType,
+  Field
+} from "type-graphql";
+import { hash, compare } from "bcryptjs";
 import { User } from "../entity/User";
+import { sign } from "jsonwebtoken";
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
 
 @Resolver()
 export class UserResolver {
@@ -30,5 +44,26 @@ export class UserResolver {
       return false;
     }
     return true;
+  }
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email", () => String) email: string,
+    @Arg("password", () => String) password: string
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      throw new Error("Invalid Credentials");
+    }
+    const valid = await compare(password, user.password);
+    if (!valid) {
+      throw new Error("Invalid Credentials");
+    }
+
+    // Login Successful
+    return {
+      accessToken: sign({ userId: user.id }, "you will never know why", {
+        expiresIn: "15m"
+      })
+    };
   }
 }
